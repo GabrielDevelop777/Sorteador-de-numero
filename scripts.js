ec5cbf4f9c0b402d85121f6e63b05ecbe3692b15
GabrielDesenvolver777 <gabrielalexok@gmail.com>
2025-02-19 22:51:48 -0300

    Atualização do codigo

--- scripts.js
+++ scripts.js
@@ -3,8 +3,16 @@ function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+    const winner = document.querySelector(".result")
+    
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    if( max > min ) {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        winner.innerHTML = result
+        winner.style.color = rgb(10, 240, 10)
+    }else {
+        winner.innerHTML = "O valor minimo tem que ser menor que o valor maximo"
+        winner.style.color = "red";
 
-    alert(result)
+    }
 }
